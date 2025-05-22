d63ef48e7a52eaa31c2c8bb38ce82584eec301c3
hiaki0617 <hiaki0617@gmail.com>
2025-05-22 17:06:40 +0900

    必殺ゲージ追加とHPゲージの修正

--- Assets/Higashi/sprict/HealthGauge.cs
+++ Assets/Higashi/sprict/HealthGauge.cs
@@ -1,19 +1,19 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class HPBar : MonoBehaviour
+public class HealthGauge : MonoBehaviour//Hpゲージの管理
 {
     public Image hpFillImage; // FillメーターのImage（バー部分）
     public float maxHP = 100f;
     private float currentHP;
 
-    void Start()
+    void Start()//開始時にHPゲージをMaXにする。
     {
         currentHP = maxHP;
         UpdateHPBar();
     }
 
-    public void TakeDamage(float damage)//ダメージを受けたときにHPが減る。
+    public void HPbarDown(float damage)//ダメージを受けたときにHPが減る。
     {
         currentHP -= damage;
         currentHP = Mathf.Clamp(currentHP, 0, maxHP);
@@ -25,11 +25,11 @@ public class HPBar : MonoBehaviour
         float fillAmount = currentHP / maxHP;
         hpFillImage.fillAmount = fillAmount;
     }
-    void Update()//ダメージを受けるために仮に作った
+    void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))//ダメージを受けるために仮に作った
         {
-            TakeDamage(5f);
+            HPbarDown(5f);
         }
     }
 }
---
+++ Assets/Higashi/sprict/SpecialGauge.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.WSA;
+
+public class SpecialGauge : MonoBehaviour//SPゲージの管理
+{
+    public Image SPFillImage; // FillメーターのImage（バー部分）
+    public float maxSP = 100f;
+    public float minSP = 0f;
+    private float currentSP;
+
+    void Start()//開始時にSPゲージ0にする。
+    {
+        currentSP = minSP;
+        UpdateSPBar();
+    }
+    public void SPbarUp(float UP)//SPバーを貯める。敵を倒した時呼び出してください。ダメージを受けたときも。
+    {
+        currentSP += UP;
+        currentSP = Mathf.Clamp(currentSP, 0, maxSP);
+        UpdateSPBar();
+    }
+    public void SPreset()//SPゲージをリセットする。技を使った後に呼び出して。 
+    {
+        currentSP = 0;
+        UpdateSPBar();
+    }
+    void UpdateSPBar()
+    {
+        float SPfillAmount = currentSP / maxSP;
+        SPFillImage.fillAmount = SPfillAmount;
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))//ダメージを受けるために仮に作った。
+        {
+            SPbarUp(1f);
+        }
+        if (Input.GetKeyDown(KeyCode.R))//Rボタンで必殺技を仮に打ったとする処理。
+        {
+            SPreset();
+        }
+    }
+}
