fe79178596efc5c7e006ee78dd013d85778aacb9
NaritaShosei <onigirikun7171@gmail.com>
2025-05-23 13:24:22 +0900

    [update]インターバルの減少

--- Assets/Higashi/sprict/Enemygenerator1.cs
+++ Assets/Higashi/sprict/Enemygenerator1.cs
@@ -6,10 +6,12 @@ public class Enemygenerator1 : MonoBehaviour
 {
     [SerializeField] private GameObject[] enemyPrefabs;    // 生成する敵のプレハブ(複数対応)
     [SerializeField] Transform[] spawnPoints;    // 敵の出現位置（複数対応）
-    [SerializeField] float spawnInterval = 3f;   // 敵を生成する間隔（秒）
-    [SerializeField] float _intervalDelta = 0.2f;
+    [SerializeField] float spawnInterval = 5f;   // 敵を生成する間隔（秒）
+    [SerializeField] float _intervalDelta = 0.5f;
     [SerializeField] float _minInterval = 1.5f;
-
+    //インターバルを減らす
+    [SerializeField] float _changeTime = 20;
+    float _currentChangeTime;
     EnemyManager _enemyManager;
 
     private float timer;
@@ -17,6 +19,7 @@ public class Enemygenerator1 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        _currentChangeTime = _changeTime;
         _enemyManager = GetComponent<EnemyManager>();
         timer = spawnInterval;//timerに生成時間を代入。
     }
@@ -24,15 +27,21 @@ public class Enemygenerator1 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!_enemyManager.IsEnemyMax()) return;
+        if (_enemyManager.IsEnemyMax()) return;
+
+        if (GameManager.Instance.Timer >= _currentChangeTime)
+        {
+            _currentChangeTime += _changeTime;
+            spawnInterval = Mathf.Max(spawnInterval - _intervalDelta, _minInterval);
+        }
         timer -= Time.deltaTime;
 
         if (timer <= 0f)
         {
             SpawnEnemy();
-            //Intervalを徐々に減らし、加減で止める
-            spawnInterval = Mathf.Max(spawnInterval - _intervalDelta, _minInterval);
             timer = spawnInterval;
+            //Intervalを徐々に減らし、加減で止める
+
         }
 
     }
