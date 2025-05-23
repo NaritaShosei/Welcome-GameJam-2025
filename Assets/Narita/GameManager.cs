2c0966c24dbb847ca1ad9d477862d3efc24005b6
NaritaShosei <onigirikun7171@gmail.com>
2025-05-23 12:57:41 +0900

    [fix]実行順変更

--- Assets/Narita/GameManager.cs
+++ Assets/Narita/GameManager.cs
@@ -10,7 +10,7 @@ public class GameManager : MonoBehaviour
 
     public bool IsCountUp => _timer >= 20;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         Instance = this;
     }
